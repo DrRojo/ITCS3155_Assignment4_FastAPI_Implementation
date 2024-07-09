4a7734b1fbee69fe7848fe210ffdf008ecdc19a2
C Berns <christopher.d.berns@gmail.com>
2024-07-08 23:45:04 -0400

    Add 1.5 of main.py to conform to Part2 instructions

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ from typing import Union
 from fastapi import FastAPI
 from pydantic import BaseModel
 
-app = FastAPI()
 
 class Item(BaseModel):
     name: str
@@ -11,16 +10,19 @@ class Item(BaseModel):
     is_offer: Union[bool, None] = None
 
 
+app = FastAPI()
+
+
 @app.get("/")
-def read_root():
+async def read_root():
     return {"Hello": "World"}
 
 
 @app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
+async def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
 
 
 @app.put("/items/{item_id}")
-def update_item(item_id: int, item: Item):
+async def create_item(item_id: int, item: Item):
     return {"item_name": item.name, "item_id": item_id}
