b8300ea996951fce07bdd81956a03f44755e1b3f
C Berns <christopher.d.berns@gmail.com>
2024-07-08 23:39:22 -0400

    Add 1.3 of main.py to conform to Part2 instructions

--- main.py
+++ main.py
@@ -21,6 +21,6 @@ def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
 
 
-@app.post("/items/{item_id}")
+@app.put("/items/{item_id}")
 def update_item(item_id: int, item: Item):
     return {"item_name": item.name, "item_id": item_id}
