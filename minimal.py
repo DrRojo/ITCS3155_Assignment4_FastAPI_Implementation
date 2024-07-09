c6d0f6e2e85c1e0a5c64e13709d7505f107addf8
C Berns <christopher.d.berns@gmail.com>
2024-07-09 00:19:52 -0400

    Correcting main.py to conform to Part2 instructions

---
+++ minimal.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+class Item(BaseModel):
+    name: str
+    price: float
+
+@app.put("/items/{item_id}")
+async def update_item(item_id: int, item: Item):
+    return {"item_name": item.name, "item_id": item_id}
